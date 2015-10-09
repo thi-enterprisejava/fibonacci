e474a425c72b115156e357ed60f1137a946aa1cd
Michael Schoderer <schoderer@googlemail.com>
2015-10-09 11:13:53 +0200

    Implemented fibonacci

--- src/main/java/com/github/thienterprisejava/App.java
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -6,8 +6,17 @@ package com.github.thienterprisejava;
  */
 public class App 
 {
+
+
+
+        public static long fib(int n) {
+            if (n <= 1) return n;
+            else return fib(n-1) + fib(n-2);
+        }
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( fib(13));
     }
 }

38a0e521ffb3a966e5a24a86d46fd3cae087f4fd
zirkumflex <philipp@charizard.local>
2015-10-09 11:19:44 +0200

    Implemented fibonacci logic

--- src/main/java/com/github/thienterprisejava/App.java
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -8,6 +8,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for (int i =0; i < 10; ++i)
+            System.out.println(fibonacci(i));
+    }
+
+    public static int fibonacci(int number) {
+        if (number<=0) {
+            return 0;
+        } else if (number == 1) {
+            return 1;
+        } else {
+            return fibonacci(number-2) + fibonacci(number-1);
+        }
     }
 }

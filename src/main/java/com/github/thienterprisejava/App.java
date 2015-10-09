6047a7105876d5932299d1c8447007f86a781d1c
Michael Obermüller <obermueller.michael@googlemail.com>
2015-10-09 11:28:20 +0200

    Update comments

--- src/main/java/com/github/thienterprisejava/App.java
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -12,17 +12,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Eingabe der Zahl" );
+        System.out.println( "Eingabe der zu berechnenden Fibonacci-Zahl:" );
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
+            // console input
             String input = reader.readLine();
             long value = Long.parseLong(input);
+
+            // calculate fibonacci of input number
             long result = fibonacci(value);
+
+            // show fibonacci result
             System.out.println("Ergebnis von Fib(" +value+ ") = " +result);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Fehler bei der Eingabe!");
         }
-
     }
 
     public static long fibonacci(long number)

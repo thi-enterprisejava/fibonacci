629e0dd6d31a748c4d84a6d8f43fcaf763ada6d6
Michael Schoderer <schoderer@googlemail.com>
2015-10-09 11:17:23 +0200

    Formatted...

--- src/main/java/com/github/thienterprisejava/App.java
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -10,8 +10,12 @@ public class App
 
 
         public static long fib(int n) {
-            if (n <= 1) return n;
-            else return fib(n-1) + fib(n-2);
+            if (n <= 1) {
+                return n;
+            }
+            else {
+                return fib(n-1) + fib(n-2);
+            }
         }
 
 

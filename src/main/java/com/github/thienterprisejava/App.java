f40167ea51b493b4cbb03c3dc5077167d8da33ec
Kufseb <sebastian.kuffer@googlemail.com>
2015-10-09 11:40:07 +0200

    Fibunacci Implementation Update

--- src/main/java/com/github/thienterprisejava/App.java
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -3,41 +3,46 @@ package com.github.thienterprisejava;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+
 
 /**
- * Hello world!
+ * Fibonacci Calculation
  *
  */
 public class App
 {
 
 
-    static int computeFib(int i)
+    static Long computeFibonacci(long i)
     {
         if(i<=0) {
-            return 0;
+            return 0l;
         }else if(i==1){
-            return 1;
+            return 1l;
         }
     else
-        return computeFib(i - 2)+ computeFib(i - 1);
+        return computeFibonacci(i - 2)+ computeFibonacci(i - 1);
     }
 
 
 
     public static void main( String[] args )throws IOException
     {
-        System.out.println( "Fibunacci Calculation" );
+        System.out.println( "Fibonacci Calculation" );
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter Number:");
         try{
-            int i = Integer.parseInt(br.readLine());
-            System.out.println("Fibunacci-Number for "+i+": "+computeFib(i));
+            Long i = Long.parseLong(br.readLine());
+            System.out.println("Calculation Fibonacci-Number for input "+i);
+            System.out.println("Fibonacci-Number for "+i+": "+ computeFibonacci(i));
+
         }catch(NumberFormatException nfe){
             System.err.println("Invalid Format!");
         }
+
+
+        System.out.println("Shutting Down");
     }
 
 

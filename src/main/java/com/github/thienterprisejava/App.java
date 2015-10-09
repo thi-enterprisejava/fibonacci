d34cbbcab403b3688f376ad8d1c0e2f1045578c9
Kufseb <sebastian.kuffer@googlemail.com>
2015-10-09 11:23:48 +0200

    Fibunacci Implementation

--- src/main/java/com/github/thienterprisejava/App.java
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -1,13 +1,46 @@
 package com.github.thienterprisejava;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+
+
+    static int computeFib(int i)
     {
-        System.out.println( "Hello World!" );
+        if(i<=0) {
+            return 0;
+        }else if(i==1){
+            return 1;
+        }
+    else
+        return computeFib(i - 2)+ computeFib(i - 1);
     }
+
+
+
+    public static void main( String[] args )throws IOException
+    {
+        System.out.println( "Fibunacci Calculation" );
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter Number:");
+        try{
+            int i = Integer.parseInt(br.readLine());
+            System.out.println("Fibunacci-Number for "+i+": "+computeFib(i));
+        }catch(NumberFormatException nfe){
+            System.err.println("Invalid Format!");
+        }
+    }
+
+
+
+
 }

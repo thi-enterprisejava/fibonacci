94e27fd7c17663d762a330b8c9dfed76085c8e28
Jochen Christ <Jochen.Christ@senacor.com>
2015-10-09 11:01:55 +0200

    Maven Structure created

---
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -0,0 +1,13 @@
+package com.github.thienterprisejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

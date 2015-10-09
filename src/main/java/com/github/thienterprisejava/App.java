81cf9dfee8857c28712a7a98fb34b573944b7de1
Daniel <thesorekz@gmail.com>
2015-10-09 11:33:10 +0200

    Implement Fibonacci

--- src/main/java/com/github/thienterprisejava/App.java
+++ src/main/java/com/github/thienterprisejava/App.java
@@ -2,12 +2,18 @@ package com.github.thienterprisejava;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static int fibonacci(int zahl) {
+        if(zahl <= 2) {
+            return 1;
+        }
+        return fibonacci(zahl - 1) + fibonacci(zahl - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Fibonacci von 7 ist: " + fibonacci(7));
     }
 }
